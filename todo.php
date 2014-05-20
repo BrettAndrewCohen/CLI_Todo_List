01e6659761a3a6acaf247bb11c1293f9438a43da
Brett Cohen <brettgmf@Bretts-MacBook-Air.local>
2014-05-20 14:29:17 -0500

    updated to do list

--- todo.php
+++ todo.php
@@ -1,24 +1,14 @@
 <?php
 
-// List array items formatted for CLI
-// Return string of list items separated by newlines.
-// Should be listed [KEY] Value like this:
-// [1] TODO item 1
-// [2] TODO item 2 - blah
-// DO NOT USE ECHO, USE RETURN
-
 function list_items($list) {
 
     $listofitems = '';
 
     foreach ($list as $key => $listitem) {
-        if($key == 0) {
-            $key = 1;
-        } else {
-            $key++;
-        }
-        $listofitems .= "[{$key}] {$listitem}\n";
-        }
+      
+      $listofitems .= "[" . ($key + 1) . "]" . $listitem . PHP_EOL;
+    
+    }
     return $listofitems;
 }
 
@@ -31,8 +21,25 @@ function get_input($upper = false) {
     // return strtoupper($result);
     // } 
     // else {
-    // $result;
+    // return $result;
     // }
+}
+
+function sort_menu($items) {
+    echo '(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered : ';
+
+    $input = get_input(TRUE);
+
+        if ($input == 'A') {
+            asort($items);
+        } elseif ($input == 'Z') {
+            arsort($items);
+        } elseif ($input == 'O') {
+            ksort($items);
+        } elseif ($input == 'R') {
+            krsort ($items);
+        }
+    return $items;
 }
 
 // Create array to hold list of todo items
@@ -45,7 +52,7 @@ do {
 echo list_items($items);
  
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (Q)uit, (S)ort, : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
@@ -55,19 +62,33 @@ echo list_items($items);
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
+        $listitem = get_input();
+        echo 'Would you like to put this item at the (B)eginning? ';
+        $input = get_input(TRUE);
         // Add entry to list array
-        $items[] = get_input();
+        if($input == 'B') {
+            array_unshift($items, $listitem);
+        } else {
+            //this adds the item to the end of an array. 
+            $items[] = $listitem;
+        }
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
         $key = get_input();
         // Remove from array
-        unset($items[--$key]);
+        unset($items[$key -1]);
         // Reset all the items in the array to start at index 1
         $items = array_values($items);
-
+    } elseif ($input == 'S') {
+        $items = sort_menu($items);
+    } elseif ($input == 'F') {
+        array_shift($items);
+    } elseif ($input == 'L') {
+        array_pop($items);
     }
+
 // Exit when input is (Q)uit
 } while($input != 'Q');
 
